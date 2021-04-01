2c6bd81dd3439ef1eafd06043391dd2fa6033575
Code Sandbox <hello@codesandbox.io>
2021-04-01 06:44:56 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.8346a783b0887dc00c1c1931b4dfe251.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "93cb2d82b113f7c7b4da",
+    "url": "https://veto1017.github.io/csb-mgg6e/static/css/main.47f405b2.chunk.css"
+  },
+  {
+    "revision": "93cb2d82b113f7c7b4da",
+    "url": "https://veto1017.github.io/csb-mgg6e/static/js/main.93cb2d82.chunk.js"
+  },
+  {
+    "revision": "ce1d3370f450d250e997",
+    "url": "https://veto1017.github.io/csb-mgg6e/static/js/1.ce1d3370.chunk.js"
+  },
+  {
+    "revision": "1245f7c5f0ea1e994ce1",
+    "url": "https://veto1017.github.io/csb-mgg6e/static/js/runtime~main.1245f7c5.js"
+  },
+  {
+    "revision": "e8d0f59bb73ded6b9915808f1e555523",
+    "url": "https://veto1017.github.io/csb-mgg6e/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "https://veto1017.github.io/csb-mgg6e/precache-manifest.8346a783b0887dc00c1c1931b4dfe251.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("https://veto1017.github.io/csb-mgg6e/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
